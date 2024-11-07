229f30b1bd6d99dcce0443601866c83b3af4be5b
Muhammad Musaib Khan <Musaib.Khan@ad.jbs.live>
2024-11-07 13:17:57 +0500

    remove max-width

--- index.php
+++ index.php
@@ -5,23 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JBS Consulting</title>
     <style>
-        body, html {
+        body {
             margin: 0;
             padding: 0;
-            height: 100%;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #000; /* Optional, dark background */
+            height: 100vh;
             overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #000;
         }
 
         img {
             width: 100%;
             height: 100%;
-            object-fit: cover; /* Fills the entire screen, cropping the image if necessary */
-            object-position: center; /* Ensures the image is centered */
+            object-fit: cover; /* Ensures the image fills the screen */
         }
     </style>
 </head>
